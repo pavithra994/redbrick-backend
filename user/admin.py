b63a168e8eebe87a980531dfcf3dba3bce599901
pavithra <pavithra.blog@gmail.com>
2022-09-08 12:18:03 +0530

    docker integration

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from user.forms import UserAdminChangeForm, UserAdminCreationForm
+from user.models import *
+
 
 # Register your models here.
+# admin.site.register(Hotel_user_table)
+class UserAdmin(BaseUserAdmin, admin.ModelAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email','fullName','role')
+    list_filter = ('role','active')
+    ordering = ('id',)
+
+    fieldsets = (
+        (None,{'fields': ('email', 'password')}),
+
+        ('Personal Details',{'fields': ('fullName', 'contactNumber')}),
+    )
+
+    add_fieldsets = (
+            (None, {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2','role')}
+            ),
+        )
+
+
+admin.site.register(User, UserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+from user.manager import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    fullName = models.CharField(max_length=150, blank=True)
+    __phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    contactNumber = models.CharField(validators=[__phone_regex], null=True, max_length=15)
+
+    __selectRoles = (
+    ('admin', 'admin'), ('staff', 'staff'),('client','client'))
+    role = models.CharField(max_length=20, choices=__selectRoles, default='client')
+
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)  # a admin user; non super-user
+    admin = models.BooleanField(default=False)  # a superuser
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Email & Password are required by default.
+
+    objects = UserManager()
+
+    # To create table name as "user" to be consistance on sharing objects with other databases.
+    class Meta:
+        db_table = "User"
+        verbose_name = 'User'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # check if user has a HID assigned to the profile.
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+    @property
+    def is_active(self):
+        "Is the user active"
+        return self.active
+
