32f611ca871abb1a4ea5718e5b2e55e60503b5f2
Pavithra Weerapperuma <36977042+pavithra994@users.noreply.github.com>
2022-09-30 01:24:42 +0530

    views implementation

--- redbrick/urls.py
+++ redbrick/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -38,4 +40,4 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/job-types', core_views.job_types),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
