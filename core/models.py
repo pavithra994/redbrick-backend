42439cd398846032240803a25752f31b6d5088bc
Pavithra Weerapperuma <36977042+pavithra994@users.noreply.github.com>
2022-10-11 00:00:17 +0530

    models str

--- core/models.py
+++ core/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 from user.models import User
 
+
 # Create your models here.
 class JobType(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=30)
-    description = models.CharField(max_length=255,null=True,blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
 
 class Client(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class StaffMember(models.Model):
     STAFF_ROLE = [
-        ("ADMIN","Admin Staff"),
-        ("MANAGER","Manager"),
-        ("SUPERVISOR","Supervisor"),
+        ("ADMIN", "Admin Staff"),
+        ("MANAGER", "Manager"),
+        ("SUPERVISOR", "Supervisor"),
     ]
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    role = models.CharField(max_length=100,choices=STAFF_ROLE,default="SUPERVISOR")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, choices=STAFF_ROLE, default="SUPERVISOR")
+
 
 # class Guest(models.Model):
 #     full_name = models.CharField(max_length=255)
@@ -30,24 +37,23 @@ class Request(models.Model):
         ("REJECTED", "REJECTED"),
         ("ON_PROGRESS", "ON_PROGRESS"),
     ]
-    job_type = models.ForeignKey("JobType",on_delete=models.CASCADE,related_name="request_related")
+    job_type = models.ForeignKey("JobType", on_delete=models.CASCADE, related_name="request_related")
     guest_name = models.CharField(max_length=255)
-    guest_email = models.EmailField(null=True,blank=True)
-    guest_phone = models.CharField(max_length=30,null=True,blank=True)
-    client = models.ForeignKey("Client",on_delete=models.CASCADE,null=True,blank=True)
+    guest_email = models.EmailField(null=True, blank=True)
+    guest_phone = models.CharField(max_length=30, null=True, blank=True)
+    client = models.ForeignKey("Client", on_delete=models.CASCADE, null=True, blank=True)
     is_reviewed = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
-    reviewed_by =  models.ForeignKey("StaffMember",on_delete=models.CASCADE,null=True,blank=True)
-    status = models.CharField(max_length=100,choices=STATUS_CHOICES,default="PENDING")
-    created_date = models.DateTimeField(auto_now_add=True,blank=True)
-
+    reviewed_by = models.ForeignKey("StaffMember", on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="PENDING")
+    created_date = models.DateTimeField(auto_now_add=True, blank=True)
 
 
 class Project(models.Model):
-    manager = models.ForeignKey("StaffMember",on_delete=models.CASCADE)
-    client = models.ForeignKey("Client",on_delete=models.CASCADE)
-    location = models.CharField(max_length=500,null=True,blank=True)
-    description = models.CharField(max_length=255,null=True,blank=True)
+    manager = models.ForeignKey("StaffMember", on_delete=models.CASCADE)
+    client = models.ForeignKey("Client", on_delete=models.CASCADE)
+    location = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
     created_date = models.DateTimeField(auto_created=True)
 
 
@@ -58,10 +64,10 @@ class Job(models.Model):
         ("CLOSED", "Payments closed"),
         ("ON_PROGRESS", "ON_PROGRESS"),
     ]
-    supervisor = models.ForeignKey("StaffMember",on_delete=models.CASCADE)
-    project = models.ForeignKey("Project",on_delete=models.CASCADE)
-    description = models.CharField(max_length=255,null=True,blank=True)
-    status = models.CharField(max_length=100,choices=STATUS_CHOICES,default="PENDING")
+    supervisor = models.ForeignKey("StaffMember", on_delete=models.CASCADE)
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="PENDING")
     created_date = models.DateTimeField(auto_created=True)
 
 
@@ -72,10 +78,10 @@ class Task(models.Model):
         ("CLOSED", "Payments closed"),
         ("ON_PROGRESS", "ON_PROGRESS"),
     ]
-    supervisor = models.ForeignKey("StaffMember",on_delete=models.CASCADE)
-    job = models.ForeignKey("Job",on_delete=models.CASCADE)
-    description = models.CharField(max_length=255,null=True,blank=True)
-    status = models.CharField(max_length=100,choices=STATUS_CHOICES,default="PENDING")
+    supervisor = models.ForeignKey("StaffMember", on_delete=models.CASCADE)
+    job = models.ForeignKey("Job", on_delete=models.CASCADE)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="PENDING")
     last_update = models.DateTimeField(auto_now=True)
 
 
